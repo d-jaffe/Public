6a64982f4ef7eacda9259b10f2a04e569f51e6b0
helixic <chrometix@gmail.com>
2012-09-01 16:09:40 -0500

    0.0.1

--- aes.php
+++ aes.php
@@ -1,20 +1,20 @@
 <?php
+
 $SECRET_KEY = 'xxx'; //40 character secret key
 $CREDENTIALS = 'user:pass';
 
-$encrypted_credentials = @base64_encode(mcrypt_cbc(MCRYPT_RIJNDAEL_128,substr($SECRET_KEY,0,40),$CREDENTIALS,MCRYPT_ENCRYPT));
+$encrypted_credentials = @base64_encode(mcrypt_cbc(MCRYPT_RIJNDAEL_128,substr($SECRET_KEY,0,40),$CREDENTIALS,MCRYPT_ENCRYPT, 0));
 
-$data['access_key'] = 'xxx'; //20 character key
-$data['credentials'] = $encrypted_credentials;
-$data['timestamp'] = date('c');
+$params['access_key'] = 'xxx'; //20 character key
+$params['credentials'] = $encrypted_credentials;
+$params['timestamp'] = date('c');
 
-
-$pre_signature = 'http://api.ravelry.com/authenticate.json?' . http_build_query($data);
+$pre_signature = 'http://api.ravelry.com/authenticate.json?' . http_build_query($params);
 $signature = base64_encode(hash_hmac('sha256', $pre_signature, $SECRET_KEY, true));
 
-$data['signature'] = $signature;
+$params['signature'] = $signature;
 
-$authUrl = http_build_query($data);
+$authUrl = http_build_query($params);
 $authUrl = 'http://api.ravelry.com/authenticate.json?' . $authUrl;
 
 echo($authUrl);
@@ -27,4 +27,5 @@ curl_setopt($c, CURLOPT_HEADER, 0);
 curl_exec($c);
 
 curl_close($c);
+
 ?>
